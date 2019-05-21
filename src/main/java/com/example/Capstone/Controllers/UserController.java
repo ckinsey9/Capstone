a6b5c78deb09c5cbf37a647bbf93b237b5445e8a
ckinsey9 <48170234+ckinsey9@users.noreply.github.com>
2019-05-20 22:01:36 -0500

    Ability for user to add App complete, redirects to User home page which displays all apps in table.

--- src/main/java/com/example/Capstone/Controllers/UserController.java
+++ src/main/java/com/example/Capstone/Controllers/UserController.java
@@ -1,28 +1,52 @@
 package com.example.Capstone.Controllers;
 
 import com.example.Capstone.Models.App;
+import com.example.Capstone.Models.Data.AppDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping(value="home")
 public class UserController {
 
+    @Autowired
+    private AppDao appDao;
+
     @RequestMapping(value="/{username}", method = RequestMethod.GET)
     public String userHomePage(@PathVariable String username, Model model) {
         model.addAttribute("title", "Home | " + username);
+        model.addAttribute("apps", appDao.findAll());
         model.addAttribute("username", username);
         return "User/index";
     }
 
     @RequestMapping(value="addApp/{username}", method = RequestMethod.GET)
     public String userAddApp(@PathVariable String username, Model model) {
-        model.addAttribute("title", "AddApp | " + username);
+        model.addAttribute("title", "Add App | " + username);
         model.addAttribute(new App());
         return "User/addApp";
+    }
+
+    @RequestMapping(value="addApp/{username}", method = RequestMethod.POST)
+    public String processAddApp(@PathVariable String username, @ModelAttribute @Valid App newApp,
+                                Errors errors,
+                                Model model) {
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add App | " + username);
+            model.addAttribute(newApp);
+            return "User/addApp";
+        }
+
+        appDao.save(newApp);
+        return "redirect:/home/" + username;
     }
 
 }
