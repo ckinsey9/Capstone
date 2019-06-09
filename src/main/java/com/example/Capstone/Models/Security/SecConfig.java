c17172438716e078fc56c3929dd54bfcca27605c
ckinsey9 <48170234+ckinsey9@users.noreply.github.com>
2019-06-09 10:40:52 -0500

    Final Version w/o spring security endpoints secured, version 2.

---
+++ src/main/java/com/example/Capstone/Models/Security/SecConfig.java
@@ -0,0 +1,34 @@
+package com.example.Capstone.Models.Security;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+
+@Configuration
+@EnableWebSecurity
+public class SecConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.
+                authorizeRequests()
+                .anyRequest().permitAll();
+                http.csrf().disable();
+
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+    }
+
+}
